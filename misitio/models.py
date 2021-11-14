3f0707a07f9f5b43e85c20879cbe828568d992d1
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-11-14 09:32:59 -0600

    Implementacion de Crud en GraphQl crear, actualizar, borrar en GraphQl

---
+++ misitio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Publicacion(models.Model):
+    producto = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.CharField(max_length=200)
+
+    def __str__(self):
+       return self.producto
