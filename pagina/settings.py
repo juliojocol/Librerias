d9b7435401c540f043b958c75aa6ea0c5b459281
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-11-27 10:47:24 -0600

    Conexion AnyWhere

--- pagina/settings.py
+++ pagina/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4)2=iopp0iuicxtbjb7$3x)1y7q36!f1u1041@-%hv%kd@d0%6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
--- pagina/urls.py
+++ pagina/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, views
 
 from graphene_django.views import GraphQLView
 from pagina.schema import schema
